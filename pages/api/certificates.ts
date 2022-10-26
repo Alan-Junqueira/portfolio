a3cf597791a447ada8a1ef54900fc5380795ab66
Alan Junqueira <contato.alanjunqueira@gmail.com>
2022-10-26 18:09:14 -0300

    delete api routes

--- pages/api/certificates.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import certificates from '../../src/libs/certificates.json'
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(certificates)
-}
--- pages/api/idioms-domain.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import idioms from '../../src/libs/idiomsDomain.json';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(idioms);
-}
