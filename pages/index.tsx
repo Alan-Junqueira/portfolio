8ba4148e359361bbee2ebeddb88b9b9738406c3f
Alan Junqueira <contato.alanjunqueira@gmail.com>
2022-10-26 18:00:20 -0300

    bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import type { GetServerSideProps, NextPage } from 'next';
+import type { GetServerSideProps } from 'next';
 import Head from 'next/head';
 
 import styles from '../styles/Home.module.css';
